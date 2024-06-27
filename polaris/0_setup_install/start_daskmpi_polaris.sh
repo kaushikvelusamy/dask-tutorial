f6ad0ae67ba4b5eeca9240fe2797165b617e94ea
AmakaOkafor <152440402+okaforn@users.noreply.github.com>
2024-06-27 10:11:58 -0500

    Create start_daskmpi_polaris.sh

---
+++ polaris/0_setup_install/start_daskmpi_polaris.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#step 4
+#start_daskmpi_polaris.sh
+#On a compute node, run this script to start the scheduler on that node
+NRANKS_PER_NODE=32
+NUM_NODES=$(cat $PBS_NODEFILE | wc -l)
+NTOTRANKS=$(( NUM_NODES * NRANKS_PER_NODE ))
+
+TMP_EXE=tmp_rpds.sh
+
+cat > ${TMP_EXE} << EOF
+#!/bin/bash
+if [ \$PMI_RANK == 0 ]; then
+    ./activate_rapids_env_polaris.sh ./start_daskmpi_rank.sh SCHEDULER
+else
+    ./activate_rapids_env_polaris.sh ./start_daskmpi_rank.sh
+fi
+EOF
+
+chmod 755 ${TMP_EXE}
+sleep 5
+
+mpiexec -n $NTOTRANKS --ppn $NRANKS_PER_NODE ./${TMP_EXE}
+
+rm ./${TMP_EXE}
